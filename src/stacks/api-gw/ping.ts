7d6749d59bdeaf29565931e86dd4793fe40cc128
Malanius Privierre <malaniusprivierre@gmail.com>
2023-01-29 12:28:57 +0100

    (api-gw): Extract ping functionality to own construct

---
+++ src/stacks/api-gw/ping.ts
@@ -0,0 +1,51 @@
+import * as cdk from 'aws-cdk-lib';
+import * as apigateway from 'aws-cdk-lib/aws-apigateway';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+
+import { PingFunction } from './ping-function';
+
+import { AppInfo } from '../../constants/app-info';
+
+export interface PingProps extends AppInfo {
+  api: apigateway.RestApi;
+}
+
+export class Ping extends Construct {
+  constructor(scope: Construct, id: string, props: PingProps) {
+    super(scope, id);
+
+    const { appName, appEnv, api } = props;
+    const region = cdk.Stack.of(this).region;
+    const partition = cdk.Stack.of(this).partition;
+
+    const pingHandler = new PingFunction(this, 'PingHandler', {
+      functionName: `${appName}-${appEnv}-ping`,
+    });
+    pingHandler.addToRolePolicy(
+      new iam.PolicyStatement({
+        effect: iam.Effect.ALLOW,
+        actions: ['apigateway:GET'],
+        resources: [`arn:${partition}:apigateway:${region}::/apikeys/*`],
+      })
+    );
+
+    // /ping
+    const pingResource = api.root.addResource('ping');
+    const pingIntegration = new apigateway.LambdaIntegration(pingHandler, {
+      allowTestInvoke: true,
+      passthroughBehavior: apigateway.PassthroughBehavior.NEVER,
+      requestTemplates: {
+        'application/json': '{"statusCode": 200}',
+      },
+    });
+    pingResource.addMethod('GET', pingIntegration, {
+      apiKeyRequired: true,
+      methodResponses: [
+        {
+          statusCode: '200',
+        },
+      ],
+    });
+  }
+}
