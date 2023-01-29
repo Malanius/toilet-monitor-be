f9b75c1dd4af7db8336a9bbcb7e256a1895f9b24
Malanius Privierre <malaniusprivierre@gmail.com>
2023-01-29 18:40:25 +0100

    (key-validation): Cache key names to avoid fetch witch each request

--- src/common/device-auth-middleware.ts
+++ src/common/device-auth-middleware.ts
@@ -10,6 +10,27 @@ import { logger, tracer } from './powertools';
 const apiGateway = tracer.captureAWSClient(new APIGateway());
 const KEY_PREFIX = process.env.KEY_PREFIX || 'toilet-';
 
+// TODO: maybe figure out a way to use middy cache instead of this?
+//    Documentation on this is missing, so probably would have to reverse engineer that.
+const cachedKeys: { [key: string]: string } = {};
+
+const fetchApiKey = async (callingKeyId: string): Promise<string> => {
+  logger.debug('Retrieving key ID info.');
+  const keyDetails = await apiGateway
+    .getApiKey({ apiKey: callingKeyId })
+    .promise();
+
+  // Need to check for undefined here because the API Gateway SDK doesn't
+  // althoug key name is required when creating an API key.
+  if (!keyDetails || !keyDetails.name) {
+    logger.error('Failed to load API key details!');
+    throw createError(500, 'Failed to load API key details!');
+  }
+
+  cachedKeys[callingKeyId] = keyDetails.name;
+  return keyDetails.name;
+};
+
 export const deviceAuthMiddleware = (): MiddlewareObj => {
   const validateKeyBefore = async (
     request: MiddyLikeRequest
@@ -17,32 +38,27 @@ export const deviceAuthMiddleware = (): MiddlewareObj => {
     const event = request.event as APIGatewayProxyEvent;
     const callingKeyId = event.requestContext.identity.apiKeyId;
     if (!callingKeyId) {
+      // This should not happen behind API GW as endpoinds should have API key required
+      // but may on direct invoke or test invoke without proper payload.
       logger.warn('API key ID missing!');
       throw createError(401, 'API key ID missing!');
     }
     logger.appendKeys({ callingKeyId });
 
-    logger.debug('Retrieving key ID info.');
-    const keyDetails = await apiGateway
-      .getApiKey({ apiKey: callingKeyId })
-      .promise();
-
-    // Need to check for undefined here because the API Gateway SDK doesn't
-    // althoug key name is required when creating an API key.
-    if (!keyDetails || !keyDetails.name) {
-      logger.error('Failed to load API key details!');
-      throw createError(500, 'Failed to load API key details!');
+    let keyName = cachedKeys[callingKeyId];
+    if (!keyName) {
+      keyName = await fetchApiKey(callingKeyId);
     }
-    logger.appendKeys({ callingKeyName: keyDetails.name });
+    logger.appendKeys({ callingKeyName: keyName });
 
-    if (!keyDetails.name.startsWith(KEY_PREFIX)) {
+    if (!keyName.startsWith(KEY_PREFIX)) {
       logger.warn(`Invalid key name, should start with '${KEY_PREFIX}'`);
       throw createError(403, 'Key is not authorized for this service.');
     }
     logger.debug('Key found and valid.');
 
     const context = request.context as DeviceContext;
-    context.deviceName = keyDetails.name;
+    context.deviceName = keyName;
   };
 
   return {
