1bb45d768ecc7703dc08358c20c4b0148bda817c
Malanius Privierre <malaniusprivierre@gmail.com>
2023-06-07 21:51:50 +0200

    chore(deps): bump cdk to 2.83.0

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,14 +1,16 @@
 import { awscdk } from 'projen';
 import { TrailingComma } from 'projen/lib/javascript';
 
-const cdkVersion = '2.62.2';
+const cdkVersion = '2.83.0';
 
 const project = new awscdk.AwsCdkTypeScriptApp({
-  cdkVersion,
   name: 'toilet-monitor-be',
   authorName: 'Malanius Privierre',
   authorEmail: 'malaniusprivierre@gmail.com',
   license: 'MIT',
+
+  cdkVersion,
+  cdkVersionPinning: true,
 
   defaultReleaseBranch: 'main',
   github: false, // disable workflows for now
@@ -70,8 +72,8 @@ const project = new awscdk.AwsCdkTypeScriptApp({
     'lint-staged@13.0.1',
   ],
 
+  // Defaults for new CDK 2.83.0 app
   context: {
-    // Defaults for new CDK 2.62.2 app
     '@aws-cdk/aws-lambda:recognizeLayerVersion': true,
     '@aws-cdk/core:checkSecretUsage': true,
     '@aws-cdk/core:target-partitions': ['aws', 'aws-cn'],
@@ -93,6 +95,17 @@ const project = new awscdk.AwsCdkTypeScriptApp({
     '@aws-cdk/aws-s3:serverAccessLogsUseBucketPolicy': true,
     '@aws-cdk/aws-route53-patters:useCertificate': true,
     '@aws-cdk/customresources:installLatestAwsSdkDefault': false,
+    '@aws-cdk/aws-rds:databaseProxyUniqueResourceName': true,
+    '@aws-cdk/aws-codedeploy:removeAlarmsFromDeploymentGroup': true,
+    '@aws-cdk/aws-apigateway:authorizerChangeDeploymentLogicalId': true,
+    '@aws-cdk/aws-ec2:launchTemplateDefaultUserData': true,
+    '@aws-cdk/aws-secretsmanager:useAttachedSecretResourcePolicyForSecretTargetAttachments':
+      true,
+    '@aws-cdk/aws-redshift:columnId': true,
+    '@aws-cdk/aws-stepfunctions-tasks:enableEmrServicePolicyV2': true,
+    '@aws-cdk/aws-ec2:restrictDefaultSecurityGroup': true,
+    '@aws-cdk/aws-apigateway:requestValidatorUniqueId': true,
+    '@aws-cdk/aws-kms:aliasNameRef': true,
   },
 });
 
