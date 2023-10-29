de08debe465c8269eb63a8d2c0d5c582062c416d
Malanius Privierre <malaniusprivierre@gmail.com>
2023-10-29 15:53:39 +0100

    feat(db): create DynamoDB table

---
+++ src/stacks/database/database.ts
@@ -0,0 +1,27 @@
+import * as cdk from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { Construct } from 'constructs';
+
+import { AppInfo } from '../../constants/app-info';
+
+export interface DatabaseProps extends cdk.StackProps, AppInfo {}
+
+export class Database extends cdk.Stack {
+  public readonly table: dynamodb.Table;
+  constructor(scope: Construct, id: string, props: DatabaseProps) {
+    super(scope, id, props);
+
+    this.table = new dynamodb.Table(this, 'Table', {
+      partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING },
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+    });
+
+    this.table.addGlobalSecondaryIndex({
+      indexName: 'gsi1pk-gsi1sk-index',
+      partitionKey: { name: 'gsi1pk', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'gsi1sk', type: dynamodb.AttributeType.STRING },
+      projectionType: dynamodb.ProjectionType.ALL,
+    });
+  }
+}
--- src/stacks/env-stacks.ts
+++ src/stacks/env-stacks.ts
@@ -2,6 +2,8 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
 import { ApiGw } from './api-gw/api-gw-stack';
+import { Database } from './database/database';
+
 import { AppInfo } from '../constants/app-info';
 
 interface EnvStackProps extends AppInfo {}
@@ -11,6 +13,14 @@ export class EnvStacks extends Construct {
     super(scope, id);
 
     const { appName, appEnv } = props;
+
+    new Database(this, 'database', {
+      ...props,
+      stackName: `${appName}-${appEnv}-database`,
+      tags: {
+        component: 'database',
+      },
+    });
 
     new ApiGw(this, 'api-gw', {
       ...props,
